9387dc6612cc93542b19a7aa4180b31ecdb171fc
mick-d97 <mickael.dolphin@live.fr>
2020-12-08 16:33:54 +0100

    ajout de l'exercice 7

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$lastname ='Jean';
+$firstname ='Bernard';
+$age = 22;
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercice 7 partie 1</title>
+</head>
+<body>
+<?='Bonjour'.' '.$lastname.' '.$firstname.', tu as'.' '.$age.' '.'ans'?>
+</body>
+</html>
